4945d8e8df40f5850bb9240f161108b2d4a52f1b
Dmoldd <oldtime_tula@mail.ru>
2022-10-07 10:19:26 +0300

    Поменял расположение секций. Изменил шрифты. Поработал с переводом контента на русский язык

---
+++ source/js/script.js
@@ -0,0 +1,12 @@
+
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.lenth > 0){
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.lenth; index++) {
+      const animItem = animItems[index];
+      const animItemHight = animItem.offsetHeight;
+      const animItemOffset =
+    }
+  }
+}
